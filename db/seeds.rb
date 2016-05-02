2b678d98af4c3f476abe6e2a2ddd4ca08fe86dd2
Andreza Feu <andrezafeu@gmail.com>
2016-05-02 17:19:10 -0400

    Populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+player1 = Player.create(name: 'Andreza')
+player2 = Player.create(name: 'Josh')
+player3 = Player.create(name: 'Nizar')
+
+tournament1 = Tournament.create(name: 'PlayStation')
+tournament2 = Tournament.create(name: 'Xbox')
+tournament2 = Tournament.create(name: 'Nintendo')
