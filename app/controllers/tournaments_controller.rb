b59510c2ca6e8c2ad70fc731690c0c5ede0b0ba9
Andreza Feu <andrezafeu@gmail.com>
2016-05-02 18:01:01 -0400

    Create route that responds to get request showing individual tournament info

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,4 +2,12 @@ class TournamentsController < ApplicationController
   def index
     render(:index)
   end
+  def api
+  	tournament = Tournament.find_by(id: params[:id])
+  	unless  tournament
+  		render json: {error: "tournament not found"}, status: 404
+  		return
+  	end
+  	render json: tournament
+  end
 end
