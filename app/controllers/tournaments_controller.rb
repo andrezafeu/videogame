ab371a4a313b45517932570dbb32d56f9639c2c8
Andreza Feu <andrezafeu@gmail.com>
2016-05-02 18:22:23 -0400

    Add new tournament

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -10,4 +10,14 @@ class TournamentsController < ApplicationController
   	end
   	render json: tournament
   end
+  def create
+  	tournament = Tournament.create(tournament_params)
+  	render json: tournament
+  end
+
+  private
+
+  def tournament_params
+  	params.require(:tournament).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/' => 'tournaments#index'
   get 'api/tournaments/:id' => 'tournaments#api'
+  post 'api/tournaments/' => 'tournaments#create'
 end
